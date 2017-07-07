c5833be082ff8713cb38841b0da03694cce742df
kantisubhra <subhrakantiray92@gmail.com>
2017-07-07 22:24:19 +0530

    Inital commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by subho on 18/6/17.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
